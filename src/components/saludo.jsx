3bb0626c7f7d0c017e2bb18f084c294028ff9124
davidciliberto1 <davidciliberto@gmail.com>
2020-07-23 20:04:34 -0300

    practicando

---
+++ src/components/saludo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Saludo = () => {
+    return (
+        <div>
+            <h1>HOliiiis</h1>
+            <input className='input' type='text' placeholder='saludando'></input>
+        </div>
+    )
+}
+export default Saludo;
